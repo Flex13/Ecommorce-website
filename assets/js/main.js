971e3c4777f13720787f1190473e6f59b653b07c
Sibanye <sibanyebukani01@gmail.com>
2020-05-16 11:05:04 +0200

    Front page

---
+++ assets/js/main.js
@@ -0,0 +1,83 @@
+$('')
+.not(".slick-intialized")
+.slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots:true,
+    infinite: true,
+    prevArrow: ".site-slider .slider-btn .prev",
+    nextArrow: ".site-slider .slider-btn .next"
+  });
+
+  $('.slider-two')
+.not(".slick-intialized")
+.slick({
+    centerMode: true,
+    centerPadding: '60px',
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    prevArrow: ".site-slider-two .slider-btn .prev",
+    nextArrow: ".site-slider-two .slider-btn .next",
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+
+  });
+
+  /* AMAKASI */
+
+  $('.slider-one')
+.not(".slick-intialized")
+  .slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 2,
+    prevArrow: ".site-slider .slider-btn .prev",
+    nextArrow: ".site-slider .slider-btn .next",
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+
+  });
+
+
+  
